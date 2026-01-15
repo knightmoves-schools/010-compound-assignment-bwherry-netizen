073af30554fa6c60c27cab6edb7bce034a7ed65d
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-15 12:01:27 -0600

    Fix arithmetic operations in Calculator class

--- Calculator.cs
+++ Calculator.cs
@@ -2,13 +2,17 @@
 public class Calculator
 {
     
-    public int Addition = 50 += 5;
+    public int Addition = 50;
+    Addition += 5;
     
-    public int Subtraction = 40 -= 1;
+    public int Subtraction = 40;
+    Subtraction -= 1;
     
-    public int Multiplication = 5 *= 7;
+    public int Multiplication = 5;
+    Multiplication *= 7;
     
-    public int Division = 6 /= 2;
+    public int Division = 6;
+    Division /= 2;
 
     public void Calculate(){
         // Add your code here
