edf295aa7dc19be19e9b9569012014917157499a
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-15 12:05:15 -0600

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -3,20 +3,21 @@ public class Calculator
 {
     
     public int Addition = 50;
-    int Addition += 5;
+    Addition =+ 5;
     
     public int Subtraction = 40;
-    int Subtraction -= 1;
+    Subtraction =- 1;
     
     public int Multiplication = 5;
-    int Multiplication *= 7;
+    Multiplication =* 7;
     
     public int Division = 6;
-    int Division /= 2;
+    Division =/ 2;
 
     public void Calculate(){
         // Add your code here
     }
 }
+
 
 
