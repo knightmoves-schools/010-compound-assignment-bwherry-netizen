6ae0ae56910bb5ce05310dd4e6f8eee121357ab0
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-15 11:58:15 -0600

    Update Calculator operations with assignment operators

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,17 @@
 ﻿namespace knightmoves;
 public class Calculator
 {
-    public int Addition = 50;
-    public int Subtraction = 40;
-    public int Multiplication = 5;
-    public int Division = 6;
+    
+    public int Addition = 50 += 5;
+    
+    public int Subtraction = 40 -= 1;
+    
+    public int Multiplication = 5 *= 7;
+    
+    public int Division = 6 /= 2;
 
     public void Calculate(){
         // Add your code here
     }
 }
+
